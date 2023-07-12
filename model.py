5c202002939e19771d4e09bdce30323d3f758f0d
SpandanPan <83960135+SpandanPan@users.noreply.github.com>
2023-07-12 22:54:33 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,11 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+
 class Model_Norm(nn.Module):
     def __init__(self,norm_type):
         super(Model_Norm, self).__init__()
